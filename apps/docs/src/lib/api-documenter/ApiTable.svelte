142207a498f10a62c00245162ad7abf1c8c7314c
Tasos Bekos <tbekos@gmail.com>
2022-11-19 18:48:14 +0200

    feat(docs): add API documentation

---
+++ apps/docs/src/lib/api-documenter/ApiTable.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import data from '../../docs/api.json';
+  import { getComment, getType, getParameters } from './helpers';
+
+  export let hideDefault = false;
+
+  export let definition: string;
+
+  $: params = getParameters(data as any, definition);
+
+  function getParamData(param: any) {
+    const { description = '', blockTags = [] } = getComment(param) || {};
+
+    return {
+      name: `${param.name}${param.flags.isOptional ? '?' : ''}`,
+      description: description ?? '—',
+      defaultValue: blockTags.find((t) => t.name === 'defaultValue')?.text,
+      type: getType(param),
+    };
+  }
+</script>
+
+{#if params}
+  <table class="table w-full">
+    <thead>
+      <tr>
+        <th>Property</th>
+        <th>Description</th>
+        <th>Type</th>
+        {#if !hideDefault}
+          <th>Default</th>
+        {/if}
+      </tr>
+    </thead>
+    <tbody>
+      {#each params as param}
+        {@const data = getParamData(param)}
+        <tr>
+          <td>{data.name}</td>
+          <td>{@html data.description}</td>
+          <td class="demo-val">{data.type}</td>
+          {#if !hideDefault}
+            <td class="font-mono">{@html data.defaultValue || '<div class="px-1">—</div>'}</td>
+          {/if}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+{/if}
