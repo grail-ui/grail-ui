cfcc50016a55472ec4eece95f20f733eaf3f2640
Tasos Bekos <tbekos@gmail.com>
2022-11-19 18:48:14 +0200

    feat(docs): add API documentation

--- apps/docs/src/lib/modules/clickOutside/ClickOutside.svelte
+++ apps/docs/src/lib/modules/clickOutside/ClickOutside.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import Demo from '$lib/demo/Demo.svelte';
+  import ApiTable from '$lib/api-documenter/ApiTable.svelte';
 </script>
 
 <Demo />
+
+<ApiTable definition="ClickOutsideConfig" />
