0cdbf3c02cb038e697bb0b97ed8980396af5bfe7
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2022-12-01 19:58:17 +0200

    feat(@grail-ui/svelte): add accordion module

---
+++ apps/docs/src/lib/modules/accordion/Accordion.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Demo from '$lib/demo/Demo.svelte';
+  import ApiTable from '$lib/api-documenter/ApiTable.svelte';
+</script>
+
+<Demo />
+
+<ApiTable definition="AccordionSingleConfig" />
+<ApiTable definition="AccordionMultipleConfig" />
+<ApiTable definition="AccordionReturn" hideDefault />
+<ApiTable definition="AccordionParams" hideDefault />
+<ApiTable definition="AccordionItemParams" hideDefault />
