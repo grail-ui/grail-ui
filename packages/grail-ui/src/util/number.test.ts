08cda5b344b274c655974308efa149515ce7f31d
Tasos Bekos <tbekos@gmail.com>
2022-12-04 16:12:44 +0200

    feat(@grail-ui/svelte): add `snapValueToStep` utility

--- packages/grail-ui/src/util/number.test.ts
+++ packages/grail-ui/src/util/number.test.ts
@@ -1,4 +1,4 @@
-import { clamp } from './number';
+import { clamp, snapValueToStep } from './number';
 
 describe('clamp', () => {
 	it('restrict between two min and max', () => {
@@ -17,5 +17,44 @@ describe('clamp', () => {
 		expect(clamp(-10, null, 20)).toEqual(-10);
 		expect(clamp(100, -10, null)).toEqual(100);
 		expect(clamp(20, null, null)).toEqual(20);
+	});
+});
+
+describe('snapValueToStep', () => {
+	it('should keep value if already on step', () => {
+		expect(snapValueToStep(2, 0, 10, 1)).toBe(2);
+		expect(snapValueToStep(-2, -2, 100, 10)).toBe(-2);
+		expect(snapValueToStep(94, -1, 100, 5)).toBe(94);
+	});
+
+	it('should snap value to nearest step based on min and max', () => {
+		expect(snapValueToStep(2, -0.5, 100, 3)).toBe(2.5);
+		expect(snapValueToStep(-6.2, -2.5, 100, 3)).toBe(-2.5);
+		expect(snapValueToStep(106.2, -2.5, 100, 3)).toBe(99.5);
+		expect(snapValueToStep(-0.009999, -0.5, 0.5, 0.01)).toBe(-0.01);
+		expect(snapValueToStep(-8, -100, 100, 5)).toBe(-10);
+		expect(snapValueToStep(-6, -100, 100, 5)).toBe(-5);
+		expect(snapValueToStep(3, -100, 100, 5)).toBe(5);
+		expect(snapValueToStep(2, -100, 100, 5)).toBe(0);
+		expect(snapValueToStep(-2.3, -2.67, 44, 0.5)).toBe(-2.17);
+		expect(snapValueToStep(-22.567, -23.567, 44, 1)).toBe(-22.567);
+		expect(snapValueToStep(1.56, -3.567, 10, 1)).toBe(1.433);
+		expect(snapValueToStep(1.2, -100, 100, 1)).toBe(1);
+	});
+
+	it('should snap value nearest step when min or max are `undefined`', () => {
+		expect(snapValueToStep(2, undefined, undefined, 3)).toBe(3);
+		expect(snapValueToStep(3, undefined, undefined, 3)).toBe(3);
+		expect(snapValueToStep(6, undefined, 5, 3)).toBe(3);
+		expect(snapValueToStep(4, undefined, 5, 3)).toBe(3);
+		expect(snapValueToStep(1, 3, undefined, 3)).toBe(3);
+	});
+
+	it('should snap value nearest step when min or max are `null`', () => {
+		expect(snapValueToStep(2, null, null, 3)).toBe(3);
+		expect(snapValueToStep(3, null, null, 3)).toBe(3);
+		expect(snapValueToStep(6, null, 5, 3)).toBe(3);
+		expect(snapValueToStep(4, null, 5, 3)).toBe(3);
+		expect(snapValueToStep(1, 3, null, 3)).toBe(3);
 	});
 });
--- packages/grail-ui/src/util/number.ts
+++ packages/grail-ui/src/util/number.ts
@@ -5,3 +5,42 @@ export function clamp(
 ): number {
 	return Math.min(Math.max(value, min ?? -Infinity), max ?? Infinity);
 }
+
+export function snapValueToStep(
+	value: number,
+	min: number | undefined | null,
+	max: number | undefined | null,
+	step: number
+): number {
+	const remainder = (value - (min ?? 0)) % step;
+	let snappedValue =
+		Math.abs(remainder) * 2 >= step
+			? value + Math.sign(remainder) * (step - Math.abs(remainder))
+			: value - remainder;
+
+	if (min != null) {
+		if (snappedValue < min) {
+			snappedValue = min;
+		} else if (max != null && snappedValue > max) {
+			snappedValue = min + Math.floor((max - min) / step) * step;
+		}
+	} else if (max != null && snappedValue > max) {
+		snappedValue = Math.floor(max / step) * step;
+	}
+
+	// correct floating point behavior by rounding to step/min precision
+	const precision = Math.max(getPrecision(step), getPrecision(min));
+
+	const pow = Math.pow(10, precision);
+	snappedValue = Math.round(snappedValue * pow) / pow;
+
+	return snappedValue;
+}
+
+function getPrecision(num: number | undefined | null): number {
+	if (num == null) return 0;
+
+	const string = num.toString();
+	const index = string.indexOf('.');
+	return index >= 0 ? string.length - index : 0;
+}
