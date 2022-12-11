31e912e8f09e4dca302695401e3b909af8316f6d
Tasos Bekos <tbekos@gmail.com>
2022-12-11 22:04:21 +0200

    feat(repo): upgrade dependencies

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,10 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
-		preprocess({
+		vitePreprocess({
 			postcss: true,
 		}),
 	],
--- packages/grail-ui/svelte.config.js
+++ packages/grail-ui/svelte.config.js
@@ -1,9 +1,9 @@
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import mm from 'micromatch';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	package: {
 		source: 'src',
