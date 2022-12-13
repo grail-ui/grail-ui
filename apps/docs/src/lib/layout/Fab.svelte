6b48f7480cc5bfea6aeb5e2c50f18a7dc0a99e13
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2022-12-13 11:13:06 +0200

    feat(docs): move scrolling on body

--- apps/docs/src/lib/layout/Fab.svelte
+++ apps/docs/src/lib/layout/Fab.svelte
@@ -7,7 +7,7 @@
 <button
 	id="fab"
 	on:pointerdown={() => drawerOpen.update((open) => !open)}
-	class="fixed bottom-4 right-8 z-50 flex h-16 w-16 items-center justify-center rounded-full border border-base-content bg-base-200/90 border-opacity-20 text-base-content backdrop-blur backdrop-filter transition focus:outline-none focus-visible:ring lg:hidden"
+	class="fixed bottom-4 right-4 z-50 flex h-16 w-16 items-center justify-center rounded-full border border-base-content/20 bg-base-content/20 text-base-content backdrop-blur backdrop-filter transition lg:hidden"
 	><span class="sr-only">Open site navigation</span>
 	<div class="swap {$drawerOpen ? 'swap-active' : ''}">
 		<CloseIcon class="swap-on h-8 w-8" /><LinesIcon class="swap-off h-8 w-8" />
