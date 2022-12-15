d85c86715274a5a085358bf1a1d962c33cda0919
Tasos Bekos <tbekos@gmail.com>
2022-12-15 14:16:18 +0200

    feat(docs): add SEO

--- apps/docs/src/routes/(app)/+layout.svelte
+++ apps/docs/src/routes/(app)/+layout.svelte
@@ -2,7 +2,31 @@
 	import './app.css';
 	import Header from '$lib/layout/Header.svelte';
 	import { theme } from '$lib/layout/layout.store';
+
+	const title = 'Grail UI';
+	const description =
+		'A library of accessible component primitives, actions and utilities for Svelte.';
+	const domain = 'grail-ui-docs.vercel.app';
+	const deployUrl = `https://${domain}/`;
+	const ogImageUrl = `${deployUrl}og.png`;
 </script>
+
+<svelte:head>
+	<meta name="description" content={description} />
+
+	<meta property="og:url" content={deployUrl} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={ogImageUrl} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content={domain} />
+	<meta property="twitter:url" content={deployUrl} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content={ogImageUrl} />
+</svelte:head>
 
 <div class="flex flex-col" data-theme={$theme}>
 	<div class="sticky top-0 z-40"><Header /></div>
