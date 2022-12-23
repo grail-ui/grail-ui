8a164cacbfac42441e1a3e45c330ea500d760589
Tasos Bekos <tbekos@gmail.com>
2022-12-23 15:05:29 +0200

    docs(docs): add anatomy & examples section to menu

--- apps/docs/src/lib/modules/menu/Menu.svelte
+++ apps/docs/src/lib/modules/menu/Menu.svelte
@@ -1,27 +1,25 @@
 <script lang="ts">
-	import Demo from '$lib/demo/Demo.svelte';
-	import Api from '$lib/api-documenter/Api.svelte';
-	import Dependency from '$lib/demo/Dependency.svelte';
-	import Features from '$lib/demo/Features.svelte';
-	import Accessibility from '$lib/demo/Accessibility.svelte';
-</script>
+	import ComponentLayout from '$lib/demo/ComponentLayout.svelte';
+	import Highlight from '$lib/highlight/Highlight.svelte';
+	import MenuOnSelect from './examples/MenuOnSelect.svelte';
 
-<Demo />
-
-<Features
-	list={[
+	const features = [
 		'Customization options for side, alignment, offsets, and collision handling.',
 		'Fully managed focus for seamless navigation.',
 		'Full keyboard navigation support, including automatic focusing on the first or last item.',
 		'Typeahead support for quickly focusing items by typing their text.',
 		'Support for disabled items.',
-	]}
-/>
+	];
 
-<Dependency modules={['@floating-ui/dom']} />
+	const api = [
+		{ definition: 'MenuConfig' },
+		{ definition: 'MenuReturn', hideDefault: true },
+		{ definition: 'PositioningOptions' },
+	];
 
-<Accessibility
-	keyboard={[
+	const examples = [{ component: MenuOnSelect, header: 'Listening to item selection' }];
+
+	const keyboard = [
 		{
 			key: ['Space', 'Enter'],
 			description: 'When focus is trigger element, toggles the menu.',
@@ -48,13 +46,48 @@
 			key: 'End',
 			description: 'Moves focus to the last element.',
 		},
-	]}
-/>
+	];
+</script>
 
-<Api
-	types={[
-		{ definition: 'MenuConfig' },
-		{ definition: 'MenuReturn', hideDefault: true },
-		{ definition: 'PositioningOptions' },
-	]}
-/>
+<ComponentLayout {features} {api} {examples} {keyboard} dependencies={['@floating-ui/dom']}>
+	<svelte:fragment slot="anatomy">
+		<p>Typically, a menu consists of:</p>
+
+		<ul>
+			<li><b>Trigger:</b> The element that toggles the menu.</li>
+			<li>
+				<b>Menu:</b> The container for menu items. The menu consists of:
+				<ul>
+					<li><b>Item:</b> An element used to trigger the selection.</li>
+					<li><b>Separator:</b> An optional element used to visually separate menu items.</li>
+				</ul>
+			</li>
+		</ul>
+
+		<Highlight
+			source={`<scr` +
+				`ipt>
+	import { createMenu } from '@grail-ui/svelte';
+
+	const { useTrigger, triggerAttrs, useMenu, menuAttrs, itemAttrs, open } = createMenu();
+
+	const options = [
+		{ id: "edit", label: "Edit" },
+		{ id: "delete", label: "Delete" },
+		{ id: "destroy", label: "Destroy" },
+	];
+</scr` +
+				`ipt>
+
+<button use:useTrigger {...$triggerAttrs}>Actions</button>
+
+{#if $open}
+	<ul use:useMenu {...$menuAttrs}>
+		{#each options as option (option.id)}
+			<li><a href="/" {...$itemAttrs(option.id)}>{option.label}</a></li>
+		{/each}
+	</ul>
+{/if}`}
+		/>
+	</svelte:fragment>
+</ComponentLayout>
