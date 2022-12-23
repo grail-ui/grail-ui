2dc34466ffb259a3cc7b090d58f86c3222640183
Tasos Bekos <tbekos@gmail.com>
2022-12-23 13:01:56 +0200

    refactor(docs): extract common layout for components

---
+++ apps/docs/src/lib/modules/accordion/examples/AccordionDisabled.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+<p>
+	To disable a specific accordion, pass the <code>disabled: true</code> property to the
+	<code>itemAttrs</code>.
+</p>
+<p>
+	When an accordion item is disabled, it is skipped from keyboard navigation and can't be interacted
+	with.
+	<Highlight
+		source={`<li {...$itemAttrs({ value: 'item-1', disabled: true })}>
+	<button {...$triggerAttrs('item-1')}>Button 1</button>
+	<div {...$contentAttrs('item-1')}>Content for one.</div>
+</li>`}
+		lines={[1]}
+	/>
+</p>
+
+<p>
+	You can also disable the entire accordion by passing <code>disabled: true</code> to the factory
+	function.
+	<Highlight
+		source={`createAccordion({
+	type: 'multiple',
+	defaultValue: ['item-1'],
+	disabled: true,
+})`}
+		lines={[4]}
+	/>
+</p>
---
+++ apps/docs/src/lib/modules/accordion/examples/AccordionExpandedDefault.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+Use the <code>defaultValue</code> prop to define the open item by default.
+<Highlight
+	source={`createAccordion({
+	type: 'multiple',
+	defaultValue: ['item-1'],
+})`}
+	lines={[3]}
+/>
