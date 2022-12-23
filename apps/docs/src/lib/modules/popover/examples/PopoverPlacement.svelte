e073564618a579f08d5be5bf6bf3196d2331f71d
Tasos Bekos <tbekos@gmail.com>
2022-12-23 19:24:07 +0200

    docs(docs): add anatomy & examples section to popover

---
+++ apps/docs/src/lib/modules/popover/examples/PopoverPlacement.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+To change the placement of the popover relatively to the trigger, set the <code>positioning</code>
+property.
+
+<Highlight
+	source={`createPopover({
+	positioning: {
+		placement: "top-start",
+		gutter: 10
+	},
+})`}
+	lines="2, 3, 4, 5"
+/>
---
+++ apps/docs/src/lib/modules/popover/examples/PopoverPortal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+If you want to customize the element that your popover portals into, use the <code>portal</code> and
+pass the actual element or a CSS selector.
+<Highlight
+	source={`createPopover({
+	positioning: {
+		placement: "top-start",
+	},
+	portal: '#custom',
+})`}
+	lines="5"
+/>
