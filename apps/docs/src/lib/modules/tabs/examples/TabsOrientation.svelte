7341821dca4977ff9e4d40773f68cd7f0ed04f69
Vasilis Diakomanolis <b.diakomanolis@gmail.com>
2022-12-23 17:52:38 +0200

    docs(docs): add anatomy & examples section to tabs

---
+++ apps/docs/src/lib/modules/tabs/examples/TabsOrientation.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+The default orientation of the tabs is horizontal. To change the orientation, set the <code
+	>orientation</code
+>
+property in the factory function to "vertical".
+<Highlight
+	source={`createTabs({
+  orientation: "vertical",
+})`}
+	lines={[2]}
+/>
