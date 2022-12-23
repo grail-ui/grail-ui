0cf9217b2c8b0754755aae335ff3445a9b78a596
Tasos Bekos <tbekos@gmail.com>
2022-12-23 16:34:48 +0200

    docs(docs): add anatomy & examples section to number input

--- apps/docs/src/lib/modules/numberInput/NumberInput.svelte
+++ apps/docs/src/lib/modules/numberInput/NumberInput.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	import Demo from '$lib/demo/Demo.svelte';
-	import Accessibility from '$lib/demo/Accessibility.svelte';
-	import Api from '$lib/api-documenter/Api.svelte';
-	import Features from '$lib/demo/Features.svelte';
-</script>
+	import ComponentLayout from '$lib/demo/ComponentLayout.svelte';
+	import Highlight from '$lib/highlight/Highlight.svelte';
+	import NumberInputMinMax from './examples/NumberInputMinMax.svelte';
 
-<Demo />
-
-<Features
-	list={[
+	const features = [
 		'Clamp value between a configurable minimum and maximum, and snap to step values.',
-		'Round values to a specific number of fraction digits & handle floating point rounding errors.',
+		'Round values to a specific number of fraction digits & handle rounding errors.',
 		'Press and hold the stepper buttons to continuously increment or decrement.',
 		'Use the keyboard to increment and decrement the value according to the step value.',
 		'Use the scroll wheel to increment and decrement the value.',
-	]}
-/>
+	];
 
-<Accessibility
-	keyboard={[
+	const api = [
+		{ definition: 'NumberInputConfig' },
+		{ definition: 'NumberInputReturn', hideDefault: true },
+	];
+
+	const examples = [{ component: NumberInputMinMax, header: 'Minimum and maximum' }];
+
+	const keyboard = [
 		{ key: ['ArrowUp', 'PageUp'], description: 'Increment value to the next step.' },
 		{ key: ['ArrowDown', 'PageDown'], description: 'Decrement value to the previous next step.' },
 		{
@@ -31,12 +31,42 @@
 		},
 		{ key: 'Home', description: 'Set input to minimum value.' },
 		{ key: 'End', description: 'Set input to maximum allowed value.' },
-	]}
-/>
+	];
+</script>
 
-<Api
-	types={[
-		{ definition: 'NumberInputConfig' },
-		{ definition: 'NumberInputReturn', hideDefault: true },
-	]}
-/>
+<ComponentLayout {features} {api} {examples} {keyboard}>
+	<svelte:fragment slot="anatomy">
+		<p>Typically, a number input consists of:</p>
+
+		<ul>
+			<li><b>Root:</b> The element that listens to all events.</li>
+			<li class="list-none">
+				<ul>
+					<li>
+						<b>Input:</b> The element that shows current value and allows user to type a new value.
+					</li>
+					<li><b>Label:</b> The accessible label for the input.</li>
+					<li><b>Increment:</b> The optional spin button to increment the value.</li>
+					<li><b>Decrement:</b> The optional spin button to decrement the value.</li>
+				</ul>
+			</li>
+		</ul>
+
+		<Highlight
+			source={`<scr` +
+				`ipt>
+	import { createNumberInput } from '@grail-ui/svelte';
+
+	const { useNumberInput, labelAttrs, inputAttrs, incrementAttrs, decrementAttrs, } = createNumberInput();
+</scr` +
+				`ipt>
+
+<div use:useNumberInput>
+	<label {...$labelAttrs}>Enter number:</label>
+	<button {...$decrementAttrs}>-</button>
+	<input type="text" {...$inputAttrs} bind:value />
+	<button {...$incrementAttrs}>+</button>
+</div>`}
+		/>
+	</svelte:fragment>
+</ComponentLayout>
---
+++ apps/docs/src/lib/modules/numberInput/examples/NumberInputMinMax.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Highlight from '$lib/highlight/Highlight.svelte';
+</script>
+
+The <code>min</code> and <code>max</code> can be used to limit the entered value to a specific
+range. The value will be clamped when the user blurs the input field. In addition, the increment and
+decrement buttons will be disabled when the value is within one step value from the bounds.
+
+<Highlight
+	source={`createNumberInput({
+	min: 0,
+	max: 100,
+})`}
+	lines="2, 3"
+/>
