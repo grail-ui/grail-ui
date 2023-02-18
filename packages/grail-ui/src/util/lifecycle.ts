dbc9da376b1919ae65417f2bf60a22060474a587
Tasos Bekos <tbekos@gmail.com>
2023-02-18 21:20:40 +0200

    feat(@grail-ui/svelte): add `tryOnMount` lifecycle utility

--- packages/grail-ui/src/util/lifecycle.ts
+++ packages/grail-ui/src/util/lifecycle.ts
@@ -1,4 +1,28 @@
-import { onDestroy } from 'svelte';
+import { onDestroy, onMount } from 'svelte';
+import { get_current_component } from 'svelte/internal';
+
+/**
+ * Call onMount() if it's inside a component lifecycle, if not, just call the function
+ *
+ * @param fn
+ * @param sync if set to false, it will run in the nextTick() of Vue
+ */
+export function tryOnMount(fn: () => unknown) {
+	let insideComponent = false;
+	try {
+		if (get_current_component()) {
+			insideComponent = true;
+		}
+	} catch {
+		/* empty */
+	}
+
+	if (insideComponent) {
+		onMount(fn);
+	} else {
+		fn();
+	}
+}
 
 /**
  * Call `onDestroy` if inside a component, otherwise do nothing
