bdc6da1647fa6d5b73ee0c6acabe508523109e46
Tasos Bekos <tbekos@gmail.com>
2023-07-17 14:25:36 +0300

    feat(repo): upgrade dependencies

--- packages/grail-ui/src/menu/__tests__/MenuTest.svelte
+++ packages/grail-ui/src/menu/__tests__/MenuTest.svelte
@@ -18,7 +18,7 @@
 			<li role="presentation"><a href="/1" {...$itemAttrs('1')}>Item 1</a></li>
 			<li role="presentation"><a href="/2" {...$itemAttrs('2')} data-disabled>Disabled</a></li>
 			<li role="presentation"><a href="/3" {...$itemAttrs('3')}>Item 2</a></li>
-			<li {...$separatorAttrs} />
+			<!-- <li {...$separatorAttrs}  /> -->
 			<li role="presentation"><a href="/4" {...$itemAttrs('4')}>Item 4</a></li>
 			<li role="presentation"><a href="/5" {...$itemAttrs('5')}><span>Icon</span> Item 5</a></li>
 		</ul>
